dd09ce95a221eb84ef579e52c8a6215fbe2ea846
yanis lamliji <yanis.lamliji@ynov.com>
2026-06-19 10:42:28 +0200

    WIK-DPS-TP03

--- WIK-DPS-TP02/Bonus/prog.c
+++ WIK-DPS-TP02/Bonus/prog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main(){
-int i;
 for(i; i<= 10000; i++){
     printf("%d\n", i);
 }
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from sys import argv
 from flask import Flask
 from Lib.func import Data
 import datetime as dt
+import socket
 
 app = Flask(__name__)
 
 @app.route('/ping')
 def api():
     data.increment()
+    print(socket.gethostname())
     return {}
 
 @app.route('/stats')
@@ -22,6 +24,6 @@ if __name__ == '__main__':
     name = argv[2]
     date = dt.datetime.now()
     app.run(
-        host="127.0.0.1",
+        host="0.0.0.0",
         port=argv[1],
         debug = False)
